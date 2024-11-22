63588deb0e2999667a4390f2e33195082a90b7f8
vijay <92502371+vijaytarunkoppadi@users.noreply.github.com>
2024-11-22 15:14:15 +0530

    Create Task_delete.c

---
+++ ESP32/TASk_intro/Task_delete.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+TaskHandle_t myTaskHandle = NULL;
+TaskHandle_t myTaskHandle2 = NULL;
+
+void Task1(void *arg)
+{
+    int count = 0;
+    while(1){
+        count++;
+        printf("Task1 printing..\n");
+        vTaskDelay(1000/ portTICK_RATE_MS);
+        if (count == 5)                               // after 5 counts vTaskDelete function delete the Task2
+        {
+          vTaskDelete(myTaskHandle2);
+          printf("Task2 is deleted!\n");
+        }
+    }
+}
+
+void Demo_Task2(void *arg)               // 5 times task2 printing happen at initial state 
+{
+    for(int i=0;i<5;i++){
+        printf("Task2 printing..\n");
+        vTaskDelay(1000/ portTICK_RATE_MS);
+    }
+}
+
+void app_main(void)
+{
+   xTaskCreate(Task, "Task1", 4096, NULL, 10, &myTaskHandle);
+   xTaskCreatePinnedToCore(Task2, "Task2", 4096, NULL,10, &myTaskHandle2, 1);
+ }
